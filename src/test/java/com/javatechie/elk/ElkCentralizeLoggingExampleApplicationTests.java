f386106ed89c674cc81eb08afb452abe8cd53dc5
getbharat <bhooshan.bharat90@gmail.com>
2021-05-24 16:04:28 +0530

    centralize logging in microservices

---
+++ src/test/java/com/javatechie/elk/ElkCentralizeLoggingExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javatechie.elk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElkCentralizeLoggingExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
